746e1580f4016dc12980f01dd26c13e89fa9075c
AndreZR0sa <andre.z.rosa@aluno.senai.br>
2025-12-11 08:28:21 -0300

    pag 2

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+        const sendButton = document.getElementById("send");
+
+        sendButton.addEventListener("click", function() {
+            // Redireciona para a próxima página
+            window.location.href = "index2.html";
+        });
+    });
